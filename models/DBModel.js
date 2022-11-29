089ea52c7df6c68a3686b0a631fa903eba427061
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2022-11-29 10:04:05 +0530

    Schema 1

--- models/DBModel.js
+++ models/DBModel.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+import Roles from ("../utils/Roles");
 
 const User_Schema = new Schema({
     name : {
@@ -8,7 +9,25 @@ const User_Schema = new Schema({
         required : [true, "Name is Required"],
         maxLength : [20,"Max Length is 20"],
         trim : true
-    }
+    },
+    email : {
+        type : String,
+        required : [true, "Email is Required"],
+        unique : true
+    },
+    password : {
+        type : String,
+        required : [true, "Password is Required"],
+        minLength : [8,"Min Length is 8"],
+        select : false
+    },
+    role : {
+        type : String,
+        enum :Object.values(Roles),
+        default: Roles.Customer,
+    },
+    ForgotPasswordToken : String,
+    ForgotPasswordExpiry : Date,
 })
 
 
