08b24cd3e57dd33eaae9c879d9bb105b6bedfe67
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2022-12-07 15:36:59 +0530

    Auth Fix

--- controllers/auth.Controller.js
+++ controllers/auth.Controller.js
@@ -10,9 +10,23 @@ export const SignUp = asyncHandler(
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
         throw new CustomError("Please Fill All the Details !", 401);
+    }
 
         //Check if User Exists
         const ExistingUser = await User.findOne({ email });
+        if(ExistingUser){
+            throw new CustomError("User Already Exists !", 401);
+            }
+
+            // Creating User
+            const user = await User.create({
+                name,
+                email,
+                password
+            })
+            const token = user.getJwtToken();
+            user.password = undefined;
+
+            res.cookie("token",token,cookieOptions)
     }
-}
 ) 
