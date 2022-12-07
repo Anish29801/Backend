b6977943c824dcd45a8467d2ad0a37d5057104a8
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2022-12-07 11:11:53 +0530

    Started with logic

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cookiePraser = rquire("cookie-parser")
+const cors = require("cors");
+const morgon = require("morgan");
+
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 module.exports = app;
